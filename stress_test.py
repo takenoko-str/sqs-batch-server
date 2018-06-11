d05c6dc86518ccdf711e2c3ff57bc2e432405033
Takuya Saito <takuya@inside.ai>
2018-06-11 15:02:56 +0900

    インデントを微修正

--- stress_test.py
+++ stress_test.py
@@ -5,17 +5,18 @@
 from threading import Thread
 import requests
 import time
- 
+
 # initialize the Keras REST API endpoint URL along with the input
 # image path
 KERAS_REST_API_URL = "http://localhost/predict"
-IMAGE_PATH = "jemma.png"
+IMAGE_PATH = "beagle.png"
  
 # initialize the number of requests for the stress test along with
 # the sleep amount between requests
 NUM_REQUESTS = 500
 SLEEP_COUNT = 0.05
- 
+
+
 def call_predict_endpoint(n):
     # load the input image and construct the payload for the request
     image = open(IMAGE_PATH, "rb").read()
@@ -24,7 +25,7 @@ def call_predict_endpoint(n):
     # submit the request
     r = requests.post(KERAS_REST_API_URL, files=payload).json()
  
-    # ensure the request was sucessful
+    # ensure the request was successful
     if r["success"]:
         print("[INFO] thread {} OK".format(n))
  
