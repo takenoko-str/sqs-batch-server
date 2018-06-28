e71481e2552065d9ffde8884261264cab520f7d7
takenoko-str <taku.hyogo.kei@gmail.com>
2018-06-28 14:18:06 +0900

    supervisord用に修正

--- ec2_ctrl.py
+++ ec2_ctrl.py
@@ -1,4 +1,5 @@
 import botocore.exceptions
+import time
 from aws_handler import SQS, EC2
 
 
@@ -7,17 +8,19 @@ if __name__ == '__main__':
                   'Values': ['warmup']}]
     ec2 = EC2.describe(my_filter)
     sqs = SQS.sample()
-    msg = sqs.message_size()
+    while True:
+        msg = sqs.message_size()
 
-    if int(msg) > 0:
-        try:
+        if int(msg) > 0:
+            try:
 
-            response = ec2.start()
-        except botocore.exceptions.ClientError:
-            print("error")
+                response = ec2.start()
+            except botocore.exceptions.ClientError:
+                print("error")
 
-    if int(msg) == 0:
-        try:
-            response = ec2.stop()
-        except botocore.exceptions.ClientError:
-            print("error")
+        if int(msg) == 0:
+            try:
+                response = ec2.stop()
+            except botocore.exceptions.ClientError:
+                print("error")
+        time.sleep(5)
