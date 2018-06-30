dc1c72511d30bda7f517c32e23ff7295d51d7548
takenoko-str <taku.hyogo.kei@gmail.com>
2018-06-30 15:53:48 +0900

    変数をサービスからクライアントに変更

--- aws_handler.py
+++ aws_handler.py
@@ -10,7 +10,7 @@ class SQS:
 
     def __init__(self, url):
         self.url = url
-        self.sqs = boto3.client('sqs', region_name='ap-northeast-1')
+        self.client = boto3.client('sqs', region_name='ap-northeast-1')
         self.messages = []
 
     @classmethod
@@ -18,11 +18,11 @@ class SQS:
         return cls(cls.URL)
 
     def send(self, body_message):
-        self.sqs.send_message(QueueUrl=self.url, MessageBody=body_message)
+        self.client.send_message(QueueUrl=self.url, MessageBody=body_message)
 
     def receive(self, num_messages=1):
         num_messages = num_messages % (settings.MAX_QUEUE_SIZE + 1)
-        response = self.sqs.receive_message(QueueUrl=self.url, MaxNumberOfMessages=num_messages)
+        response = self.client.receive_message(QueueUrl=self.url, MaxNumberOfMessages=num_messages)
         messages = response.get('Messages')
         if messages:
             self.messages += messages
@@ -30,10 +30,10 @@ class SQS:
 
     def delete(self, messages):
         for msg in messages:
-            self.sqs.delete_message(QueueUrl=self.url, ReceiptHandle=msg['ReceiptHandle'])
+            self.client.delete_message(QueueUrl=self.url, ReceiptHandle=msg['ReceiptHandle'])
 
     def message_size(self):
-        response = self.sqs.get_queue_attributes(
+        response = self.client.get_queue_attributes(
             QueueUrl=self.url,
             AttributeNames=['ApproximateNumberOfMessages']
         )
@@ -49,7 +49,7 @@ class S3:
     BUCKET = os.environ.get('S3_BUCKET')
 
     def __init__(self, bucket):
-        self.s3 = boto3.client('s3', region_name='ap-northeast-1')
+        self.client = boto3.client('s3', region_name='ap-northeast-1')
         self.bucket = bucket
 
     @classmethod
@@ -57,37 +57,37 @@ class S3:
         return cls(cls.BUCKET)
 
     def upload(self, src_file_path, dst_file_path):
-        self.s3.upload_file(src_file_path, self.bucket, dst_file_path)
+        self.client.upload_file(src_file_path, self.bucket, dst_file_path)
 
     def download(self, src_file_path, dst_file_path):
-        return self.s3.download_file(self.bucket, src_file_path, dst_file_path)
+        return self.client.download_file(self.bucket, src_file_path, dst_file_path)
 
     def put(self, key, body):
-        self.s3.put_object(Body=body, Bucket=self.bucket, Key=key)
+        self.client.put_object(Body=body, Bucket=self.bucket, Key=key)
 
     def get(self, key):
-        return self.s3.get_object(Bucket=self.bucket, Key=key)['Body'].read()
+        return self.client.get_object(Bucket=self.bucket, Key=key)['Body'].read()
 
 
 class EC2:
-    ec2 = boto3.client('ec2', region_name='ap-northeast-1')
+    client = boto3.client('ec2', region_name='ap-northeast-1')
 
     def __init__(self, ids):
         self.ids = ids
 
     @classmethod
     def describe(cls, filters, ids=[]):
-        response = cls.ec2.describe_instances(Filters=filters)
+        response = cls.client.describe_instances(Filters=filters)
         for reservation in response.get('Reservations', [{}]):
             for instance in reservation.get('Instances', [{}]):
                 ids.append(instance.get('InstanceId'))
         return cls(ids)
 
     def start(self):
-        self.ec2.start_instances(InstanceIds=self.ids)
+        self.client.start_instances(InstanceIds=self.ids)
 
     def stop(self):
-        self.ec2.stop_instances(InstanceIds=self.ids)
+        self.client.stop_instances(InstanceIds=self.ids)
 
 
 class SNS:
@@ -148,5 +148,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
