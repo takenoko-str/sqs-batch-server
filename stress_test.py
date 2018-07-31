e116226d6b93c9a86835c18806113ab2f7659526
takenoko-str <taku.hyogo.kei@gmail.com>
2018-07-31 16:27:13 +0900

    コメント削除。Optional指定

--- stress_test.py
+++ stress_test.py
@@ -1,54 +1,41 @@
-# USAGE
-# python stress_test.py
-
-# import the necessary packages
+# coding=utf-8
 from threading import Thread
+from typing import IO
 import requests
 import settings
 import time
 import sys
+import os
 
-# initialize the Keras REST API endpoint URL along with the input
-# image path
 IMAGE_PATH = "beagle.png"
 os.makedirs('logs', exist_ok=True)
-
-# initialize the number of requests for the stress test along with
-# the sleep amount between requests
 NUM_REQUESTS = int(sys.argv[1])
 SLEEP_COUNT = 0.05
 x = []
 
+
 def call_predict_endpoint(n):
-    # load the input image and construct the payload for the request
     with open(IMAGE_PATH, "rb") as f:
         image = f.read()
         payload = {"image": image}
     print("[INFO] thread start")
 
-    # submit the request
     r = requests.post(settings.KERAS_REST_API_URL, files=payload).json()
 
-    # ensure the request was successful
     if r["success"]:
         print("[INFO] thread {} OK".format(n))
-        with open("logs/stress_{}.log".format(n), 'a') as f:
+        with open("logs/stress_{}.log".format(n), 'a') as f:  # type: IO[str]
             print(r["predictions"], file=f)
 
-    # otherwise, the request failed
     else:
         print("[INFO] thread {} FAILED".format(n))
 
-# loop over the number of threads
 threads = []
 for i in range(0, NUM_REQUESTS):
-    # start a new thread to call the API
     t = Thread(target=call_predict_endpoint, args=(i,))
-    #t.daemon = True
+    # t.daemon = True
     threads.append(t)
     t.start()
     time.sleep(SLEEP_COUNT)
 
-# insert a long sleep so we can wait until the server is finished
-# processing the images
 time.sleep(3)
