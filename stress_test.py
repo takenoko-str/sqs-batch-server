30d8ab4000d72b71545b5e4706d42041733766cc
takenoko-str <taku.hyogo.kei@gmail.com>
2018-08-01 17:23:26 +0900

    loggingでログを出力

--- stress_test.py
+++ stress_test.py
@@ -3,6 +3,7 @@ from threading import Thread
 from typing import IO
 import requests
 import settings
+import logging
 import time
 import sys
 import os
@@ -12,23 +13,25 @@ os.makedirs('logs', exist_ok=True)
 NUM_REQUESTS = int(sys.argv[1])
 SLEEP_COUNT = 0.05
 x = []
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)s] (%(threadName)-10s) %(message)s')
 
 
 def call_predict_endpoint(n):
     with open(IMAGE_PATH, "rb") as f:
         image = f.read()
         payload = {"image": image}
-    print("[INFO] thread {} GO".format(n))
+    logging.debug("thread {} GO".format(n))
 
     r = requests.post(settings.KERAS_REST_API_URL, files=payload).json()
 
     if r["success"]:
-        print("[INFO] thread {} OK".format(n))
+        logging.debug("thread {} OK".format(n))
         with open("logs/stress_{}.log".format(n), 'a') as f:  # type: IO[str]
             print(r["predictions"], file=f)
 
     else:
-        print("[INFO] thread {} FAILED".format(n))
+        logging.debug("thread {} FAILED".format(n))
 
 threads = []
 for i in range(0, NUM_REQUESTS):
